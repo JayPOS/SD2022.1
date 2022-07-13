77fd1467bdb19006a844f788c1851a6144c2ecae
Joao Pedro Ribeiro <joaopedrori1998@gmail.com>
2022-07-12 22:16:50 -0300

    adding 2 functions about calculate pi (leibniz and nilakantha)

--- rpc-go/client/main.go
+++ rpc-go/client/main.go
@@ -10,6 +10,7 @@ func main() {
 
 	var nDigits int = 0
 	var pi float64 = 0.0
+	var option int = 0
 
 	client, err := rpc.DialHTTP("tcp", "localhost:4040")
 
@@ -17,8 +18,24 @@ func main() {
 		log.Fatal("Connection error: ", err)
 	}
 
-	fmt.Println("Digite o numero de casas que quer calcular do número PI! (tente não passar de 100)")
+	fmt.Printf("Digite o numero de passos que quer calcular do número PI! (tente não passar de 100000)\n\n")
+	fmt.Printf("Digite aqui o número de passos: ")
 	fmt.Scan(&nDigits)
-	client.Call("Server.CalculatePI", nDigits, &pi)
-	fmt.Println("pi is", pi)
+
+	fmt.Println("\nQual a função que quer utilizar?")
+	fmt.Println("1 - Leibniz")
+	fmt.Printf("2 - Nilakantha\n\n")
+	fmt.Print("Digite aqui sua opção: ")
+	fmt.Scan(&option)
+
+	switch option {
+	case 1:
+		client.Call("Server.CalculatePiLeibniz", nDigits, &pi)
+		fmt.Printf("O valor de Pi depois de %d passos usando a função de Leibniz é %f\n", nDigits, pi)
+	case 2:
+		client.Call("Server.CalculatePiNilakantha", nDigits, &pi)
+		fmt.Printf("O valor de Pi depois de %d passos usando a função de Nilakantha é %f\n", nDigits, pi)
+	default:
+		fmt.Println("Digite uma opção válida! Tente novamente.")
+	}
 }
--- rpc-go/server/main.go
+++ rpc-go/server/main.go
@@ -8,11 +8,47 @@ import (
 	"net/rpc"
 )
 
+func leibniz(steps int) float64 {
+	var pi_value float64 = 0.0
+	var d float64 = 1
+	for i := 0; i < steps; i++ {
+		if i%2 == 0 {
+			pi_value += 4 / d
+		} else {
+			pi_value -= 4 / d
+		}
+		d += 2
+	}
+	return pi_value
+}
+
+func nilakantha(steps int) float64 {
+	var pi_value float64 = 3.0
+	var d float64 = 2
+	for i := 0; i < steps; i++ {
+		if i%2 == 0 {
+			pi_value += 4 / ((d) * (d + 1) * (d + 2))
+		} else {
+			pi_value -= 4 / ((d) * (d + 1) * (d + 2))
+		}
+		d += 2
+	}
+	return pi_value
+}
+
 type Server struct {
 }
 
-func (a *Server) CalculatePI(nDigits int, pi *float64) error {
-	fmt.Printf("Well, you should calculate %d digits pi here\n", nDigits)
+func (a *Server) CalculatePiLeibniz(nDigits int, pi *float64) error {
+	fmt.Printf("Well, you should calculate %d steps pi here\n", nDigits)
+	*pi = leibniz(nDigits)
+
+	return nil
+}
+
+func (a *Server) CalculatePiNilakantha(nDigits int, pi *float64) error {
+	fmt.Printf("Well, you should calculate %d steps pi here\n", nDigits)
+	*pi = nilakantha(nDigits)
 
 	return nil
 }
